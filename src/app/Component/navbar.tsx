2958aea2391769fddd68233b592299e1c698101c
GrowthToBetter <baratrahjaga@gmail.com>
2024-09-05 08:42:32 +0700

    menambah jokes ahahaha

--- src/app/Component/navbar.tsx
+++ src/app/Component/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar(){
     return(<>
     <div className="w-screen h-fit p-3 pr-40 flex justify-between fixed bg-black bg-opacity-75 items-center z-50">
-        <div className="w-[70px] h-[70px] bg-[url('/img/favicon.png')] bg-cover bg-no-repeat mx-5"></div>
+        <div className="w-[70px] h-[70px] bg-[url('/img/favicon.jpg')] bg-cover bg-no-repeat mx-5"></div>
         <h1 className="font-light text-center items-center size=[30px] text-[30px] w-fit h-fit tracking-[0.5rem] text-white" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>OKS FIREMAN</h1>
         <div className="mr-12">
         <Link href="/" className="decoration-black mx-8">
