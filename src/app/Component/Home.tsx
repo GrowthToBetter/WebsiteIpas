b0b58e4cfaf0b332324019394d75dff9b43f03ce
GrowthToBetter <baratrahjaga@gmail.com>
2024-09-07 12:00:35 +0700

    membuat responsive

--- src/app/Component/Home.tsx
+++ src/app/Component/Home.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-[url('/img/background_Home.jpg')] relative top-0 flex justify-center flex-col items-center bg-cover bg-no-repeat">
+    <div className="w-screen h-screen bg-[url('/img/background_Home.jpg')] relative top-0 flex justify-center flex-col items-center sm:bg-cover sm:bg-no-repeat">
       <div className="w-screen h-screen bg-gradient-to-t top-0 from-black via-slate-100 to-slate-800 mix-blend-multiply absolute"></div>
       <div className="flex justify-center items-center flex-col mb-8">
-        <h1 className="text-white text-[85px] w-[780px] text-center m-5">
+        <h1 className="text-white md:text-[85px] md:w-[780px] sm:text-[60px] sm:w-[750px] text-center m-5">
           Welcome To Our Application
         </h1>
-        <p className="text-[30px] w-[700px] text-center m-5 text-white font-extralight">
+        <p className="md:text-[30px] md:w-[700px] sm:text-[20px] sm:w-[600px] text-center m-5 text-white font-extralight">
           laciniaconsequat. Donec luctus sollicitudin sapien vel tincidunt. Nunc{" "}
         </p>
       </div>
--- src/app/Component/navbar.tsx
+++ src/app/Component/navbar.tsx
@@ -2,27 +2,27 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Navbar(){
     return(<>
-    <div className="w-screen h-fit p-3 pr-40 flex justify-between fixed bg-black bg-opacity-75 items-center z-50">
-        <div className="w-[70px] h-[70px] bg-[url('/img/favicon.png')] bg-cover bg-no-repeat mx-5"></div>
-        <h1 className="font-light text-center items-center size=[30px] text-[30px] w-fit h-fit tracking-[0.5rem] text-white" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>OKS FIREMAN</h1>
-        <div className="mr-12">
-        <Link href="/" className="decoration-black mx-8">
-          <span className=" text-white inline-block m-4 h-fit w-fit text-[20px] font-[250]">
+    <div className="w-screen h-fit p-3 flex justify-between fixed bg-black bg-opacity-75 items-center z-50">
+        <div className="w-[70px] h-[70px] bg-[url('/img/favicon.png')] bg-cover bg-no-repeat mx-5 sm:opacity-0 md:opacity-100"></div>
+        <h1 className="font-light text-center items-center sm:size=[15px] sm:text-[15px] sm:tracking-[0.3rem] lg:size=[30px] lg:text-[30px] w-fit h-fit lg:tracking-[0.5rem] text-white" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>OKS FIREMAN</h1>
+        <div className="mr-12 flex justify-between">
+        <Link href="/" className="decoration-black xl:mx-8 sm:mx-3">
+          <span className=" text-white inline-block m-4 h-fit w-fit  xl:text-[20px] xl:font-[250] ">
             Home
           </span>
         </Link>
-        <Link href="/" className="decoration-black mx-8">
-          <span className=" text-white inline-block m-4 h-fit w-fit text-[20px] font-[250]">
+        <Link href="/" className="decoration-black xl:mx-8 sm:mx-3">
+          <span className=" text-white inline-block m-4 h-fit w-fit  xl:text-[20px] xl:font-[250]">
             Team
           </span>
         </Link>
-        <Link href="/" className="decoration-black mx-8">
-          <span className=" text-white inline-block m-4 h-fit w-fit text-[20px] font-[250]">
+        <Link href="/" className="decoration-black xl:mx-8 sm:mx-3">
+          <span className=" text-white inline-block m-4 h-fit w-fit  xl:text-[20px] xl:font-[250]">
             Product
           </span>
         </Link>
-        <Link href="/" className="decoration-black mx-8">
-          <span className=" text-white inline-block m-4 h-fit w-fit text-[20px] font-[250]">
+        <Link href="/" className="decoration-black xl:mx-8 sm:mx-3">
+          <span className=" text-white inline-block m-4 h-fit w-fit  xl:text-[20px] xl:font-[250]">
             About
           </span>
         </Link>
